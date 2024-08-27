0ff77290f1255a889b91765149b6c22958fd9a3e
athilaoliveira <athila.oliveira02@gmail.com>
2024-08-27 15:27:49 -0300

    feat: login v2 em andamento

--- src/pages/LoginPage/LoginForm/LoginForm.tsx
+++ src/pages/LoginPage/LoginForm/LoginForm.tsx
@@ -1,23 +1,29 @@
 import { Button } from "antd";
-import { TextInput } from "../../../components/Inputs/TextInputs/TextInput";
+import { Input } from "antd";
 import styles from "./LoginForm.module.css";
+import { MailOutlined, LockOutlined } from "@ant-design/icons";
 
 export function LoginForm() {
   return (
     <div className={styles["container"]}>
-      <div className={styles["title_container"]}>
-        <h1 className={styles["title"]}>Login</h1>
-        <h3 className={styles["subtitle"]}>Entre para continuar</h3>
-      </div>
-
       <div className={styles["input_container"]}>
         <label className={styles["label"]}>Digite seu e-mail</label>
-        <TextInput type={"email"} placeholder="Digite seu E-Mail" />
+        <Input
+          className={styles["input"]}
+          prefix={<MailOutlined />}
+          type={"email"}
+          placeholder="Digite seu e-mail"
+        />
       </div>
 
       <div className={styles["input_container"]}>
         <label className={styles["label"]}>Digite sua senha</label>
-        <TextInput type={"email"} placeholder="Digite seu E-Mail" />
+        <Input
+          className={styles["input"]}
+          prefix={<LockOutlined className={styles[""]}/>}
+          type={"password"}
+          placeholder={"Digite sua senha"}
+        />
       </div>
 
       <div>
@@ -26,7 +32,8 @@ export function LoginForm() {
           size={"large"}
           className={styles["button_login"]}
         >
-          <span className={styles["label_button"]}>ENTRAR</span>
+          ENTRAR
+          {/* <span className={styles["label_button"]}>ENTRAR</span> */}
         </Button>
       </div>
     </div>
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -4,7 +4,13 @@ import styles from "./LoginPage.module.css";
 export function LoginPage() {
   return (
     <div className={styles["container"]}>
-      <LoginForm />
+      <div className={styles["title_container"]}>
+        <h1 className={styles["title"]}>Login</h1>
+        <h3 className={styles["subtitle"]}>Entre para continuar</h3>
+      </div>
+      <div className={styles["form_container"]}>
+        <LoginForm />
+      </div>
     </div>
   );
 }
