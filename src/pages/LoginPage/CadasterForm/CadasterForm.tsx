f0985af415e2c8c37f81d338e4342f4eef719948
athilaoliveira <athila.oliveira02@gmail.com>
2024-08-30 16:05:44 -0300

    feat: add fomulario de cadastro

--- src/pages/LoginPage/CadasterForm/CadasterForm.tsx
+++ src/pages/LoginPage/CadasterForm/CadasterForm.tsx
@@ -1,25 +1,37 @@
-import { Button } from "antd";
+import { Button, Form } from "antd";
 import { Input } from "antd";
-import styles from "./LoginForm.module.css";
-import { MailOutlined, LockOutlined } from "@ant-design/icons";
+import styles from "./CadasterForm.module.css";
+import { MailOutlined, LockOutlined, UserOutlined } from "@ant-design/icons";
 
-export function LoginForm() {
+export function CadasterForm() {
   return (
     <div className={styles["container"]}>
       <div className={styles["title_container"]}>
-        <h1 className={styles["title"]}>Entrar no AgendeJá</h1>
+        <h1 className={styles["title"]}>Cadastre-se no AgendeJá</h1>
       </div>
+      <Form></Form>
+      <div className={styles["input_container"]}>
+        <label className={styles["label"]}>Digite seu nome</label>
+        <Input
+          className={styles["input"]}
+          prefix={<UserOutlined className={styles["icon_input"]} />}
+          type={"text"}
+          placeholder="Digite seu nome"
+          rootClassName={styles["input"]}
+        />
+      </div>
+
       <div className={styles["input_container"]}>
         <label className={styles["label"]}>Digite seu e-mail</label>
         <Input
           className={styles["input"]}
           prefix={<MailOutlined className={styles["icon_input"]} />}
           type={"email"}
-          placeholder="Digite seu e-mail"
+          placeholder={"Digite seu e-mail"}
+          classNames={{ input: styles["input"] }}
           rootClassName={styles["input"]}
         />
       </div>
-
       <div className={styles["input_container"]}>
         <label className={styles["label"]}>Digite sua senha</label>
         <Input
@@ -31,14 +43,13 @@ export function LoginForm() {
           rootClassName={styles["input"]}
         />
       </div>
-
       <div>
         <Button
           type="primary"
           size={"large"}
           className={styles["button_login"]}
         >
-          ENTRAR
+          {"CADASTRAR"}
         </Button>
       </div>
     </div>
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
 import { Button } from "antd";
 import { LoginForm } from "./LoginForm/LoginForm";
 import styles from "./LoginPage.module.css";
+import { CadasterForm } from "./CadasterForm/CadasterForm";
 
 export function LoginPage() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
   return (
-    <div className={styles["container"]}>
-      <div className={styles["desc_container"]}>
-        <h1 className={styles["title"]}>Bem vindo!</h1>
-        <h3 className={styles["subtitle"]}>
-          Entre com suas credenciais e faça o seu agendamento
+    <div
+      className={`${styles.container} ${
+        isSignUp ? styles["right-panel-active"] : ""
+      }`}
+    >
+      <div className={styles.desc_container}>
+        <h1 className={styles.title}>
+          {isSignUp ? "Bem vindo!" : "Olá, amigo!"}
+        </h1>
+        <h3 className={styles.subtitle}>
+          {isSignUp
+            ? "Preencha com suas informações e siga para o agendamento"
+            : "Entre com suas credenciais e faça o seu agendamento"}
         </h3>
 
         <Button
           type="primary"
           size={"large"}
-          className={styles["button_register"]}
+          className={styles.button_register}
+          onClick={() => {
+            if (isSignUp) {
+              setIsSignUp(false);
+            } else {
+              setIsSignUp(true);
+            }
+          }}
         >
-          CADASTRAR
+          {isSignUp ? "ENTRAR" : "CADASTRAR"}
         </Button>
       </div>
-      <div className={styles["form_container"]}>
-        <LoginForm />
+      <div
+        className={`${styles.form_container} ${
+          isSignUp ? styles["form-hidden"] : ""
+        }`}
+      >
+        {isSignUp ? <CadasterForm /> : <LoginForm />}
       </div>
     </div>
   );
