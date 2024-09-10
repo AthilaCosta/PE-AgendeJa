4156dc9aad7b41f43d61d2b26838182f436fde28
Tiago da Silva Barbosa <tiagoproedu@gmail.com>
2024-09-10 10:33:15 -0300

    replicando form de login no cadastro

--- src/pages/LoginPage/CadasterForm/CadasterForm.tsx
+++ src/pages/LoginPage/CadasterForm/CadasterForm.tsx
@@ -8,71 +8,126 @@ import {
 } from "@ant-design/icons";
 import { TextInput } from "../../../components/Inputs/TextInputs/TextInput";
 
+interface ICadastroData {
+  nome: string;
+  sobrenome: string;
+  email: string;
+  cpf_cnpj: string;
+  senha: string;
+  confirmacao_senha: string;
+}
+
 export function CadasterForm() {
+
+  const handleFinish = (values: ICadastroData) => {
+    console.log("Finish", values);
+  };
+
+  const handleFinishFailed = () => {
+    console.log("Failed");
+  };
+
   return (
     <div className={styles["container"]}>
       <div className={styles["title_container"]}>
         <h1 className={styles["title"]}>Cadastre-se no AgendeJá</h1>
       </div>
-      <Form className={styles["form_container"]}>
+      <Form
+        className={styles["form_container"]}
+        onFinish={handleFinish}
+        onFinishFailed={handleFinishFailed}
+        autoComplete="off"
+      >
         <TextInput
           prefix={<UserOutlined className={styles["icon_input"]} />}
           type={"text"}
           placeholder="Digite seu nome"
           label={"Nome"}
-          onChange={() => {}}
+          id={"nome"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
         <TextInput
           prefix={<UserOutlined className={styles["icon_input"]} />}
           type={"text"}
           placeholder={"Digite seu sobrenome"}
           label={"Sobrenome"}
-          onChange={() => {}}
+          id={"sobrenome"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
         <TextInput
           prefix={<MailOutlined className={styles["icon_input"]} />}
           type={"email"}
           placeholder={"Digite seu e-mail"}
           label={"E-mail"}
-          onChange={() => {}}
+          id={"email"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
         <TextInput
           prefix={<IdcardOutlined className={styles["icon_input"]} />}
           type={"text"}
           placeholder={"Digite seu CPF ou CNPJ"}
           label={"CPF/CNPJ"}
-          onChange={() => {}}
+          id={"cpf_cnpj"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
         <TextInput
           prefix={<LockOutlined className={styles["icon_input"]} />}
           type={"password"}
           placeholder={"Digite sua senha"}
           label={"Senha"}
-          onChange={() => {}}
+          id={"senha"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
         <TextInput
           prefix={<LockOutlined className={styles["icon_input"]} />}
           type={"password"}
           placeholder={"Repita sua senha"}
           label={"Confirme sua senha"}
-          onChange={() => {}}
+          id={"confirmacao_senha"}
+          onChange={() => { }}
           customContainerClassName={styles["input"]}
+          validation={{
+            required: true,
+            message: "Campo obrigatório",
+          }}
         />
-      </Form>
-      <div>
+
+        <Form.Item>
         <Button
-          type="primary"
+          type={"primary"}
           size={"large"}
           className={styles["button_login"]}
+          htmlType={"submit"}
         >
-          {"CADASTRAR"}
+          CADASTRAR
         </Button>
-      </div>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
